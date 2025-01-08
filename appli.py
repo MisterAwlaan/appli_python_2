b99953ddbc75730f717bedc9f7810ed00e68580a
MonsieurPrime <nidri@guardiaschool.fr>
2025-01-08 12:37:26 +0100

    ajout de fonctionnalité pour la fonction stat

--- appli.py
+++ appli.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import matplotlib
+import matplotlib.pyplot as plt
 import csv
 import hashlib
 from tkinter import *
@@ -626,10 +626,24 @@ def commande():
 
 ### fonction qui permet d'afficher les stats des commandes 
 def afficher_stat(pseudo):
+    liste1 = []
+    liste2 = []
+    count = 0
     with open('index.json','r')as fichier :
-        pass
-    pass
+        lecture = json.load(fichier)
+    for i in range(0,len(lecture)-1,1) : 
+        if lecture[i]["Vendeur"] == pseudo: 
+            liste1.append(lecture[i]["nom du produit"])
+    
+    
+    
+    '''plt.plot(liste1,liste2)
+    plt.show()'''    
+
+    
+      
+            
 
 
 # Script principal
-commande()
+afficher_stat("Mister")
