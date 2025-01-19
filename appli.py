9d6554489429a0768f739458caecf30e3b3085a5
MisterAwlaan <nidri@guardiaschool.fr>
2025-01-19 12:19:27 +0100

    modification de la fonction afficher_liste

--- appli.py
+++ appli.py
@@ -147,10 +147,17 @@ def soumettre_inscription():
     else:
         messagebox.showerror("Échec de l'inscription", "Impossible de créer le compte.")
 # Fonction pour inscrire un nouvel utilisateur
-def afficher_liste(root):
+def afficher_liste(root,pseudo):
+    contenue_final = []
     with open('produit.txt', 'r') as fichier:
-        contenu = fichier.read()
-    messagebox.showinfo("Liste des articles", contenu)
+        f = fichier.read()
+        contenu = extraire(f)
+        for i in range(len(contenu)):
+            if contenu[i][0] == pseudo:
+                contenue_final.append(contenu[i])
+        y = [','.join(i) for i in contenue_final]
+        result = '\n'.join(y)
+    messagebox.showinfo("Liste des articles",result)
 
 # Ajouter d'autres fonctions selon ton besoin...
 
@@ -206,10 +213,17 @@ def menu_utilisateur(pseudo, mots_de_passe):
 
 
 # Fonction pour afficher la liste des articles
-def afficher_liste(root):
+def afficher_liste(root,pseudo):
+    contenue_final = []
     with open('produit.txt', 'r') as fichier:
-        contenu = fichier.read()
-    messagebox.showinfo("Liste des articles", contenu)
+        f = fichier.read()
+        contenu = extraire(f)
+        for i in range(len(contenu)):
+            if contenu[i][0] == pseudo:
+                contenue_final.append(contenu[i])
+        y = [','.join(i) for i in contenue_final]
+        result = '\n'.join(y)
+    messagebox.showinfo("Liste des articles",result)
 
 # Fonction pour créer le formulaire d'ajout de produit
 def formulaire_ajouter_produit(pseudo):
